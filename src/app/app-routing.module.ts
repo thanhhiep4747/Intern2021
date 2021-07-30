85c774251b03f8ecd04ab454f4dde071e4bd1121
linhkhmt <linhnho151999@gmail.com>
2021-07-30 17:25:18 +0700

    add some component

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { OurClientsComponent } from './our-clients/our-clients.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'about-us',component:AboutUsComponent},
+  {path:'contact-us',component:ContactUsComponent},
+  {path:'our-clients',component:OurClientsComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
