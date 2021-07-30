3a85432ac0e28e4b30473ccf3321811cb3c7616d
Nam <66206021+OhaN2k@users.noreply.github.com>
2021-07-30 20:17:00 +0700

    finish requirements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
+import { OurClientsComponent } from './our-clients/our-clients.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about-us', component: AboutUsComponent},
+  {path: 'our-clients', component: OurClientsComponent},
+  {path: 'contact-us', component: ContactUsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { OurClientsComponent } from './our-clients/our-clients.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    OurClientsComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
