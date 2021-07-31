30bf74a593047d78332bdd83858ad1d69e3e06f5
hoaison5259 <hoaison5259@gmail.com>
2021-07-31 22:04:13 +0700

    Implementation for all components

--- angular-task1/src/app/home/home.component.ts
+++ angular-task1/src/app/home/home.component.ts
@@ -11,5 +11,16 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  article_titles = ["The Flight", "The City", "The Island", "The Food"];
+  article_contents = ["About the flight, lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero quo adipisci porro! Repudiandae aspernatur libero animi quam? Impedit omnis eligendi, deleniti quidem perferendis assumenda cumque sapiente voluptatum non, a iusto.",
+                      "About the city, lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero quo adipisci porro! Repudiandae aspernatur libero animi quam? Impedit omnis eligendi, deleniti quidem perferendis assumenda cumque sapiente voluptatum non, a iusto.",
+                      "About the island, lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero quo adipisci porro! Repudiandae aspernatur libero animi quam? Impedit omnis eligendi, deleniti quidem perferendis assumenda cumque sapiente voluptatum non, a iusto.",
+                      "About the food, lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero quo adipisci porro! Repudiandae aspernatur libero animi quam? Impedit omnis eligendi, deleniti quidem perferendis assumenda cumque sapiente voluptatum non, a iusto."];
+  article_title = this.article_titles[0];
+  content = this.article_contents[0];
 
+  chooseArticle(index: number){
+    this.article_title = this.article_titles[index];
+    this.content = this.article_contents[index];
+  }
 }
