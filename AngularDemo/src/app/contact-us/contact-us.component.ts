3ea3746de2cd24920f2aa69953d121c9f7ddba9d
tmsang <minhsang.75.got@gmail.com>
2021-08-01 23:32:09 +0700

    Finish screen AboutUs

--- AngularDemo/src/app/contact-us/contact-us.component.ts
+++ AngularDemo/src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
+  name! : string;
+  email = '';
+  phone = '';
+  message = '';
 
-  constructor() { }
+
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
+  }
+
+  submitForm(): void {
+    alert()
   }
 
 }
