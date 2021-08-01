dac0693ee3e0e7e354c6b468e6348c2615a289a3
linhkhmt <linhnho151999@gmail.com>
2021-08-01 11:33:08 +0700

    change some component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { OurClientsComponent } from './our-clients/our-clients.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   {path:'home',component:HomeComponent},
   {path:'about-us',component:AboutUsComponent},
   {path:'contact-us',component:ContactUsComponent},
